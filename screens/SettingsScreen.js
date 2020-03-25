95d3941e0d76eb97bc542aa9b6e86d7221439b91
TheIncredibles3445 <62172317+TheIncredibles3445@users.noreply.github.com>
2020-03-25 16:58:48 +0300

    testing

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import React from "react";
+// import { ExpoConfigView } from "@expo/samples";
+import { View, Text } from "react-native";
 
 export default function SettingsScreen() {
-  /**
-   * Go ahead and delete ExpoConfigView and replace it with your content;
-   * we just wanted to give you a quick view of your config.
-   */
-  return <ExpoConfigView />;
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
 }
 
 SettingsScreen.navigationOptions = {
-  title: 'app.json',
+  title: "app.json"
 };
